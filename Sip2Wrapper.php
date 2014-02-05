40af58bc36b03783721fb504f5acad62205e3367
Nathan Johnson <njohnson@ena.com>
2014-02-05 10:36:51 -0600

    Adding comments

--- Sip2Wrapper.php
+++ Sip2Wrapper.php
@@ -1,7 +1,44 @@
 <?php
 
-include 'sip2.class.php';
+require_once 'sip2.class.php';
+
+
+/**
+ *
+ * @author njohnson@ena.com
+ */
+
+/**
+ * This is a wrapper class for the sip2.class.php from google code
+ *
+ * usage:
+ *
+ * require_once 'Sip2Wrapper.php';
+ * $sip2 = new Sip2Wrapper(
+ *  array(
+ *      'hostname' => $hostname,
+ *      'port' => 6001,
+ *      'withCrc' => false,
+ *      'location' => $location,
+ *      'institutionId' => $institutionId
+ *  )
+ * );
+
+ * $sip2->login($user, $pass);
+
+ * if ($sip2->startPatronSession($patron, $patronpwd)) {
+ *   var_dump($sip2->patronScreenMessages);
+ * }
+ *
+ */
+
 class Sip2Wrapper {
+
+    /**
+     *
+     * protected variables, accessible read-only via magic getter method
+     * For instance, to get a copy of $_sip2, you can call $obj->sip2
+     */
     protected $_sip2 = NULL;
     protected $_connected = false;
     protected $_selfChecked = false;
@@ -10,7 +47,15 @@ class Sip2Wrapper {
     protected $_patronInfo = NULL;
     protected $_acsStatus = NULL;
 
+
+    /**
+     *
+     * @param string $name - the member variable name
+     * @throws Exception
+     * @return mixed
+     */
     public function __get($name) {
+        /* look for a getter function named getName */
         $functionName = 'get'.ucfirst($name);
         if (method_exists($this, $functionName)) {
             return call_user_func(array($this, $functionName));
@@ -18,10 +63,21 @@ class Sip2Wrapper {
         throw new Exception('Undefined parameter '.$name);
     }
 
+    /**
+     * getter function for $this->_sip2
+     * @return sip2
+     */
+
     public function getSip2() {
         return $this->_sip2;
     }
-    
+
+
+    /**
+     *
+     * @throws Exception if patron session hasn't began
+     * @return array
+     */
     public function getPatronStatus() {
         if (!$this->_inPatronSession) {
             throw new Exception('Must start patron session before calling getPatronStatus');
@@ -33,6 +89,11 @@ class Sip2Wrapper {
         return $this->_patronStatus;
     }
 
+
+    /**
+     * parses patron status to determine if login was successful.
+     * @return boolean returns true if valid, false otherwise
+     */
     public function getPatronIsValid() {
         $patronStatus = $this->getPatronStatus();
         if (strcmp($patronStatus['variable']['BL'][0], 'Y') !== 0 || strcmp($patronStatus['variable']['CQ'][0], 'Y') !== 0) {
@@ -40,7 +101,11 @@ class Sip2Wrapper {
         }
         return true;
     }
-    
+
+    /**
+     * Returns the total fines from patron status call
+     * @return number the float value of the fines
+     */
     public function getPatronFinesTotal() {
         $status = $this->getPatronStatus();
         if (isset($status['variable']['BV'][0])) {
@@ -48,6 +113,13 @@ class Sip2Wrapper {
         }
         return 0.00;
     }
+
+    /**
+     * returns the Screen Messages field of the patron status, which can include
+     * for example blocked or barred
+     *
+     * @return array the screen messages
+     */
 
     public function getPatronScreenMessages() {
         $status = $this->getPatronStatus();
@@ -58,7 +130,11 @@ class Sip2Wrapper {
             return array();
         }
     }
-    
+
+    /**
+     * gets the patron info hold items field
+     * @return array Hold Items
+     */
     public function getPatronHoldItems() {
         $info = $this->fetchPatronInfo('hold');
         if (isset($info['variable']['AS'])) {
@@ -67,6 +143,10 @@ class Sip2Wrapper {
         return array();
     }
 
+    /**
+     * Get the patron info overdue items field
+     * @return array overdue items
+     */
     public function getPatronOverdueItems() {
         $info = $this->fetchPatronInfo('overdue');
         if (isset($info['variable']['AT'])) {
@@ -74,6 +154,11 @@ class Sip2Wrapper {
         }
         return array();
     }
+
+    /**
+     * get the charged items field
+     * @return array charged items
+     */
 
     public function getPatronChargedItems() {
         $info = $this->fetchPatronInfo('charged');
@@ -83,6 +168,10 @@ class Sip2Wrapper {
         return array();
     }
 
+    /**
+     * return patron fine detail from patron info
+     * @return array fines
+     */
     public function getPatronFineItems() {
         $info = $this->fetchPatronInfo('fine');
         if (isset($info['variable']['AV'])) {
@@ -90,7 +179,11 @@ class Sip2Wrapper {
         }
         return array();
     }
-    
+
+    /**
+     * return patron recall items from patron info
+     * @return array patron items
+     */
     public function getPatronRecallItems() {
         $info = $this->fetchPatronInfo('recall');
         if (isset($info['variable']['BU'])) {
@@ -98,7 +191,13 @@ class Sip2Wrapper {
         }
         return array();
     }
-    
+
+
+    /**
+     * return patron unavailable items from patron info
+     * @return array unavailable items
+     */
+
     public function getPatronUnavailableItems() {
         $info = $this->fetchPatronInfo('unavail');
         if (isset($info['variable']['CD'])) {
@@ -107,6 +206,12 @@ class Sip2Wrapper {
         return array();
     }
 
+    /**
+     * worker function to call out to sip2 server and grab patron information.
+     * @param string $type One of 'none', 'hold', 'overdue', 'charged', 'fine', 'recall', or 'unavail'
+     * @throws Exception if startPatronSession has not been called with success prior to calling this
+     * @return array the parsed response from the server
+     */
     public function fetchPatronInfo($type = 'none') {
         if (!$this->_inPatronSession) {
             throw new Exception('Must start patron session before calling fetchPatronInfo');
@@ -122,11 +227,21 @@ class Sip2Wrapper {
         $this->_patronInfo[$type] = $info_response;
         return $info_response;
     }
-
+    /**
+     * getter for acsStatus
+     * @return Ambigous <NULL, multitype:string multitype:multitype:  >
+     */
     public function getAcsStatus() {
         return $this->_acsStatus;
     }
 
+    /**
+     * constructor
+     * @param $sip2Params array of key value pairs that will set the corresponding member variables
+     * in the underlying sip2 class
+     * @param boolean $autoConnect whether or not to automatically connect to the server.  defaults
+     * to true
+     */
     public function __construct($sip2Params = array(), $autoConnect = true) {
         $sip2 = new sip2;
         foreach ($sip2Params as $key => $val) {
@@ -147,7 +262,12 @@ class Sip2Wrapper {
             $this->connect();
         }
     }
-    
+
+    /**
+     * Connect to the server
+     * @throws Exception if connection fails
+     * @return boolean returns true if connection succeeds
+     */
     public function connect() {
         $returnVal = $this->_sip2->connect();
         if ($returnVal === true) {
@@ -158,7 +278,16 @@ class Sip2Wrapper {
         }
         return true;
     }
-    
+
+    /**
+     * authenticate with admin credentials to the backend server
+     * @param string $bindUser The admin user
+     * @param unknown $bindPass The admin password
+     * @param string $autoSelfCheck Whether to call SCStatus after login.  Defaults to true
+     * you probably want this.
+     * @throws Exception if login failed
+     * @return boolean returns true if login succeeded.
+     */
     public function login($bindUser, $bindPass, $autoSelfCheck=true) {
         $msg = $this->_sip2->msgLogin($bindUser, $bindPass);
         $login = $this->_sip2->parseLoginResponse($this->_sip2->get_message($msg));
@@ -171,8 +300,13 @@ class Sip2Wrapper {
         }
         return true;
     }
-    
+    /**
+     * Checks the ACS Status to ensure that the ACS is online
+     * @throws Exception if ACS is not online
+     * @return boolean returns true if online
+     */
     public function selfCheck() {
+
         /* execute self test */
         $msg = $this->_sip2->msgSCStatus();
         $status = $this->_sip2->parseACSStatusResponse($this->_sip2->get_message($msg));
@@ -184,20 +318,33 @@ class Sip2Wrapper {
 
         return true;
     }
-    
+
+    /**
+     * This method is required before any get/fetch methods that have Patron in the name.  Upon
+     * successful login, it sets the inPatronSession property to true, otherwise false.
+     * @param string $patronId Patron login ID
+     * @param string $patronPass Patron password
+     * @return boolean returns true on successful login, false otherwise
+     */
     public function startPatronSession($patronId, $patronPass) {
         if ($this->_inPatronSession) {
             $this->endPatronSession();
         }
         $this->_sip2->patron = $patronId;
         $this->_sip2->patronpwd = $patronPass;
+        // set to true before call to getPatronIsValid since it will throw an exception otherwise
         $this->_inPatronSession = true;
         if(!$this->getPatronIsValid()) {
+            // set to false since it failed
             $this->_inPatronSession = false;
         }
         return $this->_inPatronSession;
     }
 
+    /**
+     * method to grab the patron status from the server and store it in _patronStatus
+     * @return boolean always returns true
+     */
     public function fetchPatronStatus() {
         $msg = $this->_sip2->msgPatronStatusRequest();
         $patron = $this->_sip2->parsePatronStatusResponse($this->_sip2->get_message($msg));
@@ -206,6 +353,10 @@ class Sip2Wrapper {
         return true;
     }
 
+    /**
+     * method to send a patron session to the server
+     * @throws Exception if patron session is not properly ended
+     */
     public function endPatronSession() {
         $msg = $this->_sip2->msgEndPatronSession();
         $end = $this->_sip2->parseEndSessionResponse($this->_sip2->get_message($msg));
@@ -216,8 +367,12 @@ class Sip2Wrapper {
         $this->_patronStatus = NULL;
         $this->_patronInfo = NULL;
     }
-    
+
+    /**
+     * disconnect from the server
+     */
     public function disconnect() {
+
         $this->_sip2->disconnect();
         $this->_connected = false;
         $this->_inPatronSession = false;
